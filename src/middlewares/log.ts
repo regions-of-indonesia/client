3e34fed19be0f6af8ee0b2165acb644a7b68fdfc
flamrdevs <flamrdevs@gmail.com>
2022-09-24 09:18:32 +0700

    Add log options

--- src/middlewares/log.ts
+++ src/middlewares/log.ts
@@ -1,13 +1,23 @@
 import type { Middleware } from "../types";
 
-const log = (): Middleware => async (context, next) => {
-  const start = new Date().getTime();
-  const data = await next();
-  const end = new Date().getTime();
-
-  console.log(`[${end} | ${end - start}ms] GET ${context.url}`);
-
-  return data;
+type LogOptions = {
+  key?: boolean;
+  url?: boolean;
 };
 
+const log = (options: LogOptions = {}): Middleware => {
+  const { key = false, url = true } = options;
+
+  return async (context, next) => {
+    const start = new Date().getTime();
+    const data = await next();
+    const end = new Date().getTime();
+
+    console.log(`[${end} ${end - start}ms] ${key ? `[KEY ${context.key}]` : ""} ${url ? `[URL ${context.url}]` : ""}`);
+
+    return data;
+  };
+};
+
+export type { LogOptions };
 export { log };
