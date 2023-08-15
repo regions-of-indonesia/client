dd0b92f70042614fa5d9c813fd100d6d08878f4b
flamrdevs <flamrdevs@gmail.com>
2023-08-16 00:52:17 +0700

    refactor: internal

--- src/index.ts
+++ src/index.ts
@@ -33,39 +33,35 @@ const resolveBaseURL = (value?: CreateOptions["baseURL"]): BaseURLObject => ({
 const getDynamicURLByKey = (key: string) => key;
 const getStaticURLByKey = (key: string) => `${key}.json`;
 
-const STR = ["provinces", "districts", "subdistricts", "villages", "region", "search", "code", "name"] as const;
-
-const PATHNAME = {
-  ps: () => STR[0],
-  p: (code: string) => `${STR[0]}/${code}`,
-  ds: (code: string) => `${STR[0]}/${code}/${STR[1]}`,
-  d: (code: string) => `${STR[1]}/${code}`,
-  sds: (code: string) => `${STR[1]}/${code}/${STR[2]}`,
-  sd: (code: string) => `${STR[2]}/${code}`,
-  vs: (code: string) => `${STR[2]}/${code}/${STR[3]}`,
-  v: (code: string) => `${STR[3]}/${code}`,
-  r: (code: string) => `${STR[4]}/${code}`,
-  f: (name: string) => `${STR[5]}?${STR[7]}=${name}`,
-  fps: (name: string) => `${STR[5]}/${STR[0]}?${STR[7]}=${name}`,
-  fds: (name: string) => `${STR[5]}/${STR[1]}?${STR[7]}=${name}`,
-  fsds: (name: string) => `${STR[5]}/${STR[2]}?${STR[7]}=${name}`,
-  fvs: (name: string) => `${STR[5]}/${STR[3]}?${STR[7]}=${name}`,
-};
-
-type TypeofPATHNAME = typeof PATHNAME;
+const pathname_provinces = "provinces";
+const pathname_province = (code: string) => `provinces/${code}`;
+const pathname_districts = (code: string) => `provinces/${code}/districts`;
+const pathname_district = (code: string) => `districts/${code}`;
+const pathname_subdistricts = (code: string) => `districts/${code}/subdistricts`;
+const pathname_subdistrict = (code: string) => `subdistricts/${code}`;
+const pathname_villages = (code: string) => `subdistricts/${code}/villages`;
+const pathname_village = (code: string) => `villages/${code}`;
+const pathname_region = (code: string) => `region/${code}`;
+const pathname_search = (name: string) => `search?name=${name}`;
+const pathname_search_provinces = (name: string) => `search/provinces?name=${name}`;
+const pathname_search_districts = (name: string) => `search/districts?name=${name}`;
+const pathname_search_subdistricts = (name: string) => `search/subdistricts?name=${name}`;
+const pathname_search_villages = (name: string) => `search/villages?name=${name}`;
 
 const acceptString = (value: unknown, message: string): string => {
-    if (typeof value !== "string") throw new Error(message);
-    return value;
+    if (typeof value === "string") return value;
+    throw new Error(message);
   },
-  acceptStringRequireCode = (value: unknown) => acceptString(value, `Require ${STR[6]}`),
-  acceptStringRequireName = (value: unknown) => acceptString(value, `Require ${STR[7]}`);
+  acceptStringRequireCode = (value: unknown) => acceptString(value, `Require code`),
+  acceptStringRequireName = (value: unknown) => acceptString(value, `Require name`);
 
-const warnNotSupportStaticAPI = <T>(enable: boolean, name: string, value: T): T => {
-    if (enable) console.warn(`${name} API is not supported on static API`);
+const warnNotSupportStaticAPI = <T>(name: string, value: T): T => {
+    console.warn(`${name} API is not supported on static API`);
     return value;
   },
-  warnSearchAPI = <T>(enable: boolean, value: T): T => warnNotSupportStaticAPI(enable, "Search", value);
+  warnSearchAPI = () => warnNotSupportStaticAPI<Region[]>("Search", []);
+
+const defaultStaticSearchFn = async (_name?: string, _opts?: Options) => warnSearchAPI();
 
 const fetcher = async <T extends any>(url: string, opts?: Options): Promise<T> => {
   const response = await fetch(url, opts);
@@ -76,42 +72,48 @@ const fetcher = async <T extends any>(url: string, opts?: Options): Promise<T> =
 const betterdiff = (diff: number) =>
   diff >= 10000 ? "10+s" : diff >= 100 ? `${(diff / 1000).toFixed(1)}s` : `${" ".repeat(diff < 10 ? 1 : 0)}${diff}ms`;
 
+type FindAllFn = (opts?: Options) => Promise<Region[]>;
+type FindByCodeFn = (code?: string, opts?: Options) => Promise<Region>;
+type FindByParentCodeFn = (code?: string, opts?: Options) => Promise<Region[]>;
+type RegionFn = (code?: string, opts?: Options) => Promise<Region>;
+type SearchFn = (name?: string, opts?: Options) => Promise<Region[]>;
+
 type Client = {
   province: {
     find: {
       (opts?: Options): Promise<Region[]>;
-      by: (code?: string, opts?: Options) => Promise<Region>;
+      by: FindByCodeFn;
     };
-    search: (name?: string, opts?: Options) => Promise<Region[]>;
+    search: SearchFn;
   };
   district: {
     find: {
       (code?: string, opts?: Options): Promise<Region[]>;
-      by: (code?: string, opts?: Options) => Promise<Region>;
+      by: FindByCodeFn;
     };
-    search: (name?: string, opts?: Options) => Promise<Region[]>;
+    search: SearchFn;
   };
   subdistrict: {
     find: {
       (code?: string, opts?: Options): Promise<Region[]>;
-      by: (code?: string, opts?: Options) => Promise<Region>;
+      by: FindByCodeFn;
     };
-    search: (name?: string, opts?: Options) => Promise<Region[]>;
+    search: SearchFn;
   };
   village: {
     find: {
       (code?: string, opts?: Options): Promise<Region[]>;
-      by: (code?: string, opts?: Options) => Promise<Region>;
+      by: FindByCodeFn;
     };
-    search: (name?: string, opts?: Options) => Promise<Region[]>;
+    search: SearchFn;
   };
-  region: (code?: string, opts?: Options) => Promise<Region>;
+  region: RegionFn;
   search: {
     (name?: string, opts?: Options): Promise<Region[]>;
-    provinces: Client["province"]["search"];
-    districts: Client["district"]["search"];
-    subdistricts: Client["subdistrict"]["search"];
-    villages: Client["village"]["search"];
+    provinces: SearchFn;
+    districts: SearchFn;
+    subdistricts: SearchFn;
+    villages: SearchFn;
   };
 };
 
@@ -134,27 +136,22 @@ const create = (options?: CreateOptions) => {
     };
 
     return new Promise(async (resolve, reject) => {
-      if (typeof opts?.signal !== "undefined") {
-        opts.signal.onabort = () => {
-          reject(new Error("Aborted"));
-        };
-      }
-
+      if (typeof opts?.signal !== "undefined") opts.signal.onabort = () => reject(new Error("Aborted"));
       resolve(await runner(MIDDLEWARES));
     });
   };
 
-  const urlfeetch = async <T extends any>(key: string, url: string, opts?: Options): Promise<T> => {
+  const urlfx = async <T extends any>(key: string, url: string, opts?: Options): Promise<T> => {
     url = `${FINAL_BASE_URL}/${url}`;
 
-    const start = Date.now(),
-      log = <T>(value: T) => {
-        if (LOGGER) {
-          const end = Date.now();
-          console.log([`[${end}]`, `[${betterdiff(end - start)}]`, `[KEY ${key}]`, `[URL ${url}]`].filter(Boolean).join("  |  "));
-        }
-        return value;
-      };
+    const start = Date.now();
+    const log = <T>(value: T) => {
+      if (LOGGER) {
+        const end = Date.now();
+        console.log([`[${end}]`, `[${betterdiff(end - start)}]`, `[KEY ${key}]`, `[URL ${url}]`].filter(Boolean).join("  |  "));
+      }
+      return value;
+    };
 
     if (opts?.signal?.aborted) throw new Error("Aborted");
 
@@ -162,46 +159,51 @@ const create = (options?: CreateOptions) => {
     return log(await execute({ key, url }, async (ctx: Context) => await fetcher(ctx.url, opts), opts));
   };
 
-  const feetch = <T extends any>(key: string, opts?: Options) => urlfeetch<T>(key, getURLByKey(key), opts);
+  const fx = <T extends any>(key: string, opts?: Options) => urlfx<T>(key, getURLByKey(key), opts);
 
-  const createFindByFn = (keyofPATHNAME: keyof TypeofPATHNAME) => (code?: string, opts?: Options) =>
-      feetch<Region>(PATHNAME[keyofPATHNAME](acceptStringRequireCode(code)), opts),
-    createSearchFn = (keyofPATHNAME: keyof TypeofPATHNAME) =>
-      STATIC
-        ? async (_name?: string, _opts?: Options) => warnSearchAPI<Region[]>(LOGGER, [])
-        : (name?: string, opts?: Options) => feetch<Region[]>(PATHNAME[keyofPATHNAME](acceptStringRequireName(name)), opts);
+  const province_find = ((opts) => fx(pathname_provinces, opts)) satisfies FindAllFn;
+  const district_find = ((provinceCode, opts) =>
+    fx(pathname_districts(acceptStringRequireCode(provinceCode)), opts)) satisfies FindByParentCodeFn;
+  const subdistrict_find = ((districtCode, opts) =>
+    fx(pathname_subdistricts(acceptStringRequireCode(districtCode)), opts)) satisfies FindByParentCodeFn;
+  const village_find = ((subdistrictCode, opts) =>
+    fx(pathname_villages(acceptStringRequireCode(subdistrictCode)), opts)) satisfies FindByParentCodeFn;
 
-  const province_find = (opts?: Options) => feetch<Region[]>(PATHNAME.ps(), opts),
-    province_search = createSearchFn("fps"),
-    district_find = (provinceCode?: string, opts?: Options) => feetch<Region[]>(PATHNAME.ds(acceptStringRequireCode(provinceCode)), opts),
-    district_search = createSearchFn("fds"),
-    subdistrict_find = (districtCode?: string, opts?: Options) =>
-      feetch<Region[]>(PATHNAME.sds(acceptStringRequireCode(districtCode)), opts),
-    subdistrict_search = createSearchFn("fsds"),
-    village_find = (subdistrictCode?: string, opts?: Options) =>
-      feetch<Region[]>(PATHNAME.vs(acceptStringRequireCode(subdistrictCode)), opts),
-    village_search = createSearchFn("fvs");
+  (province_find as Client["province"]["find"]).by = ((code, opts) =>
+    fx(pathname_province(acceptStringRequireCode(code)), opts)) satisfies FindByCodeFn;
+  (district_find as Client["district"]["find"]).by = ((code, opts) =>
+    fx(pathname_district(acceptStringRequireCode(code)), opts)) satisfies FindByCodeFn;
+  (subdistrict_find as Client["subdistrict"]["find"]).by = ((code, opts) =>
+    fx(pathname_subdistrict(acceptStringRequireCode(code)), opts)) satisfies FindByCodeFn;
+  (village_find as Client["village"]["find"]).by = ((code, opts) =>
+    fx(pathname_village(acceptStringRequireCode(code)), opts)) satisfies FindByCodeFn;
 
-  province_find.by = createFindByFn("p");
-  district_find.by = createFindByFn("d");
-  subdistrict_find.by = createFindByFn("sd");
-  village_find.by = createFindByFn("v");
+  const province_search = (
+    STATIC ? defaultStaticSearchFn : (name, opts) => fx(pathname_search_provinces(acceptStringRequireName(name)), opts)
+  ) satisfies SearchFn;
+  const district_search = (
+    STATIC ? defaultStaticSearchFn : (name, opts) => fx(pathname_search_districts(acceptStringRequireName(name)), opts)
+  ) satisfies SearchFn;
+  const subdistrict_search = (
+    STATIC ? defaultStaticSearchFn : (name, opts) => fx(pathname_search_subdistricts(acceptStringRequireName(name)), opts)
+  ) satisfies SearchFn;
+  const village_search = (
+    STATIC ? defaultStaticSearchFn : (name, opts) => fx(pathname_search_villages(acceptStringRequireName(name)), opts)
+  ) satisfies SearchFn;
 
-  const province = { find: province_find, search: province_search } as Client["province"],
-    district = { find: district_find, search: district_search } as Client["district"],
-    subdistrict = { find: subdistrict_find, search: subdistrict_search } as Client["subdistrict"],
-    village = { find: village_find, search: village_search } as Client["village"],
-    region = ((code?, opts?) => feetch<Region>(PATHNAME.r(acceptStringRequireCode(code)), opts)) as Client["region"],
-    search = (
-      STATIC
-        ? async (_name?: string, _opts?: Options) => warnSearchAPI<Region[]>(LOGGER, [])
-        : (name?: string, opts?: Options) => feetch<Region[]>(PATHNAME.f(acceptStringRequireName(name)), opts)
-    ) as Client["search"];
+  const province = { find: province_find, search: province_search } as Client["province"];
+  const district = { find: district_find, search: district_search } as Client["district"];
+  const subdistrict = { find: subdistrict_find, search: subdistrict_search } as Client["subdistrict"];
+  const village = { find: village_find, search: village_search } as Client["village"];
+  const region = ((code, opts) => fx(pathname_region(acceptStringRequireCode(code)), opts)) as Client["region"];
+  const search = (
+    STATIC ? defaultStaticSearchFn : (name, opts) => fx(pathname_search(acceptStringRequireName(name)), opts)
+  ) satisfies SearchFn;
 
-  search.provinces = province_search;
-  search.districts = district_search;
-  search.subdistricts = subdistrict_search;
-  search.villages = village_search;
+  (search as Client["search"]).provinces = province_search;
+  (search as Client["search"]).districts = district_search;
+  (search as Client["search"]).subdistricts = subdistrict_search;
+  (search as Client["search"]).villages = village_search;
 
   return {
     province,
@@ -216,5 +218,5 @@ const create = (options?: CreateOptions) => {
 export type { CacheDriver } from "./middlewares";
 export { cache, createMemoryDriver, delay } from "./middlewares";
 
-export type { CreateOptions };
+export type { CreateOptions, Client };
 export { create };
